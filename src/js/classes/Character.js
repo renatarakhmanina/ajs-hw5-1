1cdd74ffb561d9b19be11dde8b8909b236968029
Renata Rakhmanina <renatarahmanina@gmail.com>
2024-03-26 16:59:29 +0100

    Type checking fixed

--- src/js/classes/Character.js
+++ src/js/classes/Character.js
@@ -3,6 +3,10 @@ export default class Character {
     if (name.length < 2 || name.length > 10) {
       throw new Error('Name length should be between 2 and 10 characters');
     }
+
+    if (!name || !type || typeof name !== 'string' || typeof type !== 'string') {
+      throw new Error('Name and type must have string values');
+    }
     
     this.name = name;
     this.type = type;
